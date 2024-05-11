a3a019d044ab89f591d3505aeb9c171b6a7863a6
sunflowercactus <sobikfox@gmail.com>
2024-05-11 12:31:41 +0300

    Added and configured Hunter

---
+++ _builds/CMakeFiles/check.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hex/sunflowercactus/workspace/projects/lab07/tests/test1.cpp" "/home/hex/sunflowercactus/workspace/projects/lab07/_builds/CMakeFiles/check.dir/tests/test1.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/hex/projects/hunter/_Base/xxxxxxx/f845a29/5a7c41e/Install/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/hex/sunflowercactus/workspace/projects/lab07/_builds/CMakeFiles/print.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ _builds/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/hex/sunflowercactus/workspace/projects/lab07
+# Build directory: /home/hex/sunflowercactus/workspace/projects/lab07/_builds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/hex/sunflowercactus/workspace/projects/lab07/_builds/check")
+set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/hex/sunflowercactus/workspace/projects/lab07/CMakeLists.txt;52;add_test;/home/hex/sunflowercactus/workspace/projects/lab07/CMakeLists.txt;0;")
